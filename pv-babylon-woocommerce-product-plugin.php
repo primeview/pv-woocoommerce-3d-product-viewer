34ab638f0df60c10c41a262e92e543355dbefecc
Yardi Fox <yardi@primeview.com>
2024-06-27 19:02:45 +0100

    local commit

---
+++ pv-babylon-woocommerce-product-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Primeview Woocommerce Product Babylon Viewer
+ * Plugin URI: https://www.primeview.com
+ * Description: Display 3D models on Woocommerce Product Details page
+ * Version: 0.31
+ * Author: Yardi Fox
+ * Author URI: https://www.primeview.com
+ * Text Domain: pv-woo-babylon
+ * GitHub Plugin URI: https://github.com/eldinor/babylon-wordpress-plugin
+ */
